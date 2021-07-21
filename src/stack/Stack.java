e394930a58a8cc4dcbc35ebaf9475960a3f27f7c
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-07-21 09:29:41 +0530

    After creating a method to return length of the stack

---
+++ src/stack/Stack.java
@@ -0,0 +1,23 @@
+package stack;
+
+import node.Node;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Abhinav Bhardwaj
+ * Date: 21-07-2021
+ * Time: 09:24
+ */
+
+public class Stack {
+    private Node top;
+    private int length;
+
+
+
+    public int length() {
+        return length;
+    }
+
+
+}
